98390d56c93051d47ad0d4d003c1532d72c539b0
zvek25 <doradokje@gmail.com>
2013-09-03 22:10:21 +0800

    commit 1 for git reset

--- GitHubTutorial/src/main/java/com/practice/FirstClass.java
+++ GitHubTutorial/src/main/java/com/practice/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First Class testing");
 		System.out.println("Second line");
+		System.out.println("Added 3rd line");
 	}
 }
