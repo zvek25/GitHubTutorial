86813cd93eb623394a1c317363f7f03dcf472a8a
zvek25 <doradokje@gmail.com>
2013-09-08 12:22:54 +0800

    add 2nd line on second class

--- GitHubTutorial/src/main/java/com/practice/SecondClass.java
+++ GitHubTutorial/src/main/java/com/practice/SecondClass.java
@@ -3,5 +3,6 @@ package com.practice;
 public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("First line for seconclass.java");
+		System.out.println("Added 2nd line on second class");
 	}
 }
